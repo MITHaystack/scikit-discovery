3210f1502f06bc09c567b0bdc18b0dd6004a3136
Cody Rude <cmrude@mit.edu>
2017-08-10 14:48:22 -0400

    Updating doxygen documentation

--- skdiscovery/fix_latex.sh
+++ skdiscovery/fix_latex.sh
@@ -9,6 +9,7 @@ gsed -ri 's/\\\+dataremover\.//g' *.tex
 gsed -ri 's/\\\+framework\.\\\+discoverypipeline\.//g' *.tex
 gsed -ri 's/\\\+gca\.//g' *.tex
 gsed -ri 's/\\\+gpshplotter\.//g' *.tex
+gsed -ri 's/\\\+calibrate\\\+\\_\\\+grace\.//g' *.tex
 gsed -ri 's/\\\+grace\.//g' *.tex
 gsed -ri 's/\\\+hcluster\.//g' *.tex
 gsed -ri 's/\\\+hyperbolictan\.//g' *.tex
@@ -26,4 +27,16 @@ gsed -ri 's/\\\+snow\\\+\\_\\\+remover\.//g' *.tex
 gsed -ri 's/\\\+stagecontainers\.//g' *.tex
 gsed -ri 's/\\\+trend\.//g' *.tex
 gsed -ri 's/\\\+variantdbscan\.//g' *.tex
-gsed -ri 's/\\\+vdbscan\.//g' *.tex
+gsed -ri 's/\\\+dbscan\.//g' *.tex
+gsed -ri 's/\\\+antenna\\\+\\_\\\+offset\.//g' *.tex
+gsed -ri 's/\\\+catalog\\\+\\_\\\+generator\.//g' *.tex
+gsed -ri 's/\\\+combine\\\+\\_\\\+columns\.//g' *.tex
+gsed -ri 's/\\\+data\\\+\\_\\\+generator\.//g' *.tex
+gsed -ri 's/\\\+geolocation\.//g' *.tex
+gsed -ri 's/\\\+midas\.//g' *.tex
+gsed -ri 's/\\\+outlier\.//g' *.tex
+gsed -ri 's/\\\+propagate\\\+\\_\\\+nans\.//g' *.tex
+gsed -ri 's/\\\+skew\.//g' *.tex
+gsed -ri 's/\\\+stabilization\.//g' *.tex
+gsed -ri 's/\\\+table\\\+\\_\\\+filter\.//g' *.tex
+gsed -ri 's/\\\+weighted\\\+\\_\\\+average\.//g' *.tex
