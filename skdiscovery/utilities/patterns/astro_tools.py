afc21288ebe9abba7b4ad3bb6a0bfcdfbef631d5
Cody Rude <cmrude@mit.edu>
2017-10-16 09:29:56 -0400

    Updating license information

--- skdiscovery/utilities/patterns/astro_tools.py
+++ skdiscovery/utilities/patterns/astro_tools.py
@@ -1,3 +1,6 @@
+# The following license applies to angular_seperation, abs_mag,
+# app_mag, and move_point
+
 # The MIT License (MIT)
 # Copyright (c) 2017 Massachusetts Institute of Technology
 #
@@ -23,6 +26,39 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+
+# The functions z_to_v, v_to_z, lf, dlf, cdf_dlf, and inv_cdf_dlf are
+# adapted from R code written by Cody Rude. The function NFW was
+# adapted from python code written by Cody Rude. The original versions
+# of these are licensed under the MIT license below. Any changes made
+# to the original version are copyright MIT and follow the license and
+# copyright listed above.
+
+
+# The MIT License (MIT)
+# Copyright (c) 2016 Cody Rude
+#
+# Author: Cody Rude
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 
 import numpy as np
 import math
