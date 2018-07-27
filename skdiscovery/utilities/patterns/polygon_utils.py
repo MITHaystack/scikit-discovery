11e9dacdca5ca13e708b9e6f9fde9ebf318bc415
Cody Rude <cmrude@mit.edu>
2018-07-27 11:45:29 -0400

    Added missing documentation

--- skdiscovery/utilities/patterns/polygon_utils.py
+++ skdiscovery/utilities/patterns/polygon_utils.py
@@ -3,12 +3,30 @@ from shapely.geometry import Polygon, Point
 from collections import OrderedDict
 
 def shoelaceArea(in_vertices):
+    """
+    Determine the area of a polygon using the shoelace method
+
+    https://en.wikipedia.org/wiki/Shoelace_formula
+
+    @param in_vertices: The vertices of a polygon. 2d Array where the first column is the
+                        x coordinates and the second column is the y coordinates
+
+    @return: Area of the polygon
+    """
     x = in_vertices[:,0]
     y = in_vertices[:,1]
     return 0.5 *(np.sum(x * np.roll(y,shift=-1)) - np.sum(np.roll(x,shift=-1) * y))
 
 
 def parseBasemapShape(aquifers, aquifers_info):
+    """
+    Create shapely polygons from shapefile read in with basemap
+
+    @param aquifers: Data read in shapefile from basemap
+    @param aquifers_info: Metadata read from shapefile from basemap
+
+    @return: Dictionary containing information about shapes and shapely polygon of shapefile data
+    """
     polygon_data = []
     test_list = []
 
@@ -33,7 +51,15 @@ def parseBasemapShape(aquifers, aquifers_info):
 
 
 def nearestEdgeDistance(x,y,poly):
+    """
+    Determine the distance to the closest edge of a polygon
 
+    @param x: x coordinate
+    @param y: y coordinate
+    @param poly: Shapely polygon
+
+    @return distance from x,y to nearest edge of the polygon
+    """
     point = Point(x,y)
 
     ext_dist = poly.exterior.distance(point)
@@ -47,6 +73,14 @@ def nearestEdgeDistance(x,y,poly):
 
 
 def findPolygon(in_data, in_point):
+    """
+    Find the polygon that a point resides in
+
+    @param in_data: Input data containing polygons as read in by parseBasemapShape
+    @param in_point: Shapely point
+
+    @return: Index of shape in in_data that contains in_point
+    """
     result_num = None
     for index, data in enumerate(in_data):
         if data['polygon'].contains(in_point):
@@ -60,6 +94,14 @@ def findPolygon(in_data, in_point):
     return result_num
 
 def getInfo(row, key, fill, polygon_data):
+    """
+    Retrieve information from polygon data:
+
+    @param row: Container with key 'ShapeIndex'
+    @param key: Key of data to retrieve from polygon_data element
+    @param fill: Value to return if key does not exist in polygon_data element
+    @param polygon_data: Polygon data as read in by parseBasemapShape
+    """
     try:
         return polygon_data[int(row['ShapeIndex'])]['info'][key]
     except KeyError:
@@ -67,6 +109,14 @@ def getInfo(row, key, fill, polygon_data):
 
 
 def findClosestPolygonDistance(x,y,polygon_data):
+    """
+    Find the distance to the closest polygon
+
+    @param x: x coordinate
+    @param y: y coordinate
+    @param polygon_data: Polygon data as read in by parseBasemapShape
+    @return Distance from x, y to the closest polygon polygon_data
+    """
     min_dist = np.inf
     shape_index = -1
     point = Point(x,y)
